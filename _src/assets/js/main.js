1ce6c550c6301054251f50e17f6dda304409e670
laurageeme <registro.laura@gmail.com>
2020-03-19 21:58:02 +0100

    function clicklisteners done js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,7 +6,8 @@ const favElem = document.querySelector('.favourites');
 const inputValue = document.querySelector('.searchInput');
 const searchButton = document.querySelector('.buttonSearch');
 
-let tvShowsList = null;
+let tvShowsList = [];
+let favourites = [];
 
 //1.Función llamar a la Api
 
@@ -26,8 +27,32 @@ function conectToApi() {
 
 function renderTvShows(tvShowsArr) {
   for (let item of tvShowsArr) {
-    ulElem.innerHTML += `<li id='${item.show.id}' class='tvShow-list_item'><img src='${item.show.image.medium}' alt='Poster'</img><p class='show-title'>${item.show.name}</p></li>`;
+    if (item.show.image !== null) {
+      ulElem.innerHTML += `<li id='${item.show.id}' class='tvShow-list_item'><img src='${item.show.image.medium}' alt='Poster'</img><p class='show-title'>${item.show.name}</p></li>`;
+    } else {
+      ulElem.innerHTML += `<li id='${item.show.id}' class='tvShow-list_item'><img src='https://via.placeholder.com/210x295/ffffff/666666/?text=TV' alt='Poster'</img><p class='show-title'>${item.show.name}</p></li>`;
+    }
+    addClickListeners();
   }
 }
 
+//3.Función para añadir listener a los li para guardar en favoritos. La ejecutamos donde pinta las películas, es decir en el paso 2.
+
+function addClickListeners() {
+  const tvShowLiElements = document.querySelectorAll('.tvShow-list_item');
+
+  for (let tvShowLi of tvShowLiElements) {
+    tvShowLi.addEventListener('click', saveFavourites);
+  }
+}
+
+
+
+
+
+
+
+function saveFavourites() {
+  
+}
 searchButton.addEventListener('click', conectToApi);
